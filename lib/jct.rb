c763a6568fb7e60c728b3d1ddf2ed5fec83c1d91
mi-wada <mtak.wd@gmail.com>
2022-04-27 15:31:11 +0900

    Refactored by creating Term, Amount and ExciseTaxRate classes

--- lib/jct.rb
+++ lib/jct.rb
@@ -1,6 +1,9 @@
 require 'date'
 require 'bigdecimal'
 require 'bigdecimal/util'
+require 'jct/excise_tax_rate'
+require 'jct/term'
+require 'jct/amount'
 require 'jct/version'
 
 module Jct
@@ -11,57 +14,37 @@ module Jct
   RATE105 = 1.05r.freeze
   RATE108 = 1.08r.freeze
   RATE110 = 1.10r.freeze
-  EXCISE_HASHES = [
+  EXCISE_TAX_RATES = [
     # 1873/1/1 is the date when Japan changed its calendar to the solar calendar (Meiji era).
-    { rate: RATE100, start_on: Date.new(1873, 1, 1), end_on: Date.new(1989, 3, 31) },
-    { rate: RATE103, start_on: Date.new(1989, 4, 1), end_on: Date.new(1997, 3, 31) },
-    { rate: RATE105, start_on: Date.new(1997, 4, 1), end_on: Date.new(2014, 3, 31) },
-    { rate: RATE108, start_on: Date.new(2014, 4, 1), end_on: Date.new(2019, 9, 30) },
-    # If we were to use Date::Infinity.new for end_on, an exception would occur in the later calculation, 
+    ExciseTaxRate.new(rate: RATE100, term: Term.new(start_on: Date.new(1873, 1, 1), end_on: Date.new(1989, 3, 31))),
+    ExciseTaxRate.new(rate: RATE103, term: Term.new(start_on: Date.new(1989, 4, 1), end_on: Date.new(1997, 3, 31))),
+    ExciseTaxRate.new(rate: RATE105, term: Term.new(start_on: Date.new(1997, 4, 1), end_on: Date.new(2014, 3, 31))),
+    ExciseTaxRate.new(rate: RATE108, term: Term.new(start_on: Date.new(2014, 4, 1), end_on: Date.new(2019, 9, 30))),
+    # If we were to use Date::Infinity.new for end_on, an exception would occur in the later calculation,
     # so here we will use a date far in the future.
-    { rate: RATE110, start_on: Date.new(2019, 10, 1), end_on: Date.new(2999, 1, 1) }
+    ExciseTaxRate.new(rate: RATE110, term: Term.new(start_on: Date.new(2019, 10, 1), end_on: Date.new(2999, 1, 1)))
   ]
 
-  private_constant :EXCISE_HASHES
+  private_constant :EXCISE_TAX_RATES, :ExciseTaxRate, :Term, :Amount
 
   def amount_with_tax(amount, date: Date.today, fraction: :truncate)
     return amount if amount < 0
 
-    (BigDecimal("#{amount}") * rate(date)).__send__(fraction)
+    (BigDecimal(amount.to_s) * rate(date)).__send__(fraction)
   end
 
   def yearly_amount_with_tax(amount:, start_on:, end_on:, fraction: :truncate)
-    # You can convert Integer/BigDecimal/Float/String/Rational classes to Rational,
-    # but the `amount` keyword argument does not accept BigDeciaml, Float and String for the following reasons.
-    #   - Rational objects may be implicitly converted to BigDecimal type when performing arithmetic operations using BigDecimal and Rational.
-    #     - Also, when you try to convert BigDecimal to Rational, the resulting value may not be Rational, but BigDecimal.
-    #   - Float is not accepted because it is not suitable for calculating sales tax rates.
-    #   - String is not accepted because an exception is raised by data that cannot be converted, such as 1.1.1, for example.
-    raise ArgumentError.new('amount data-type must be Integer or Rational') unless amount.is_a?(Integer) || amount.is_a?(Rational)
-    raise ArgumentError.new('start_on data-type must be Date') unless start_on.is_a?(Date)
-    raise ArgumentError.new('end_on data-type must be Date') unless end_on.is_a?(Date)
-    raise ArgumentError.new('start_on must not be after than end_on') if start_on > end_on
-    return amount if amount < 0
+    amount = Amount.new(amount)
+    return amount.value if amount.value < 0
 
-    daily_amount = Rational(amount, (start_on..end_on).count)
+    term = Term.new(start_on: start_on, end_on: end_on)
 
-    EXCISE_HASHES.inject(0) do |sum, hash|
-      # It determines whether there are overlapping periods by comparing the start and end dates of a certain consumption tax with 
-      # the start and end dates of the period for which the tax-inclusive price is to be calculated this time.
-      # If there is an overlap, the tax-inclusive price is calculated by multiplying the consumption tax rate for the applicable period
-      # by the number of days and pro rata amount for the overlapping period.
-      larger_start_on = [start_on, hash[:start_on]].max
-      smaller_end_on = [end_on, hash[:end_on]].min
-
-      # Check if there is an overlapping period
-      if larger_start_on <= smaller_end_on
-        # Number of days of overlapping period
-        number_of_days_in_this_excise_rate_term = (larger_start_on..smaller_end_on).count
-
-        sum += (daily_amount * number_of_days_in_this_excise_rate_term * hash[:rate]).__send__(fraction)
-      end
-
-      sum
+    EXCISE_TAX_RATES.inject(0) do |sum, excise_tax_rate|
+      sum + (
+        amount.per_day(term.number_of_days) *
+        term.number_of_days_that_overlap_with(excise_tax_rate.term) *
+        excise_tax_rate.rate
+      ).__send__(fraction)
     end
   end
 
@@ -74,65 +57,44 @@ module Jct
   # and there are other charges that should be combined (e.g., the annual basic fee and the optional fee),
   # if this method returns the amount including tax, it cannot be combined with the other charges.
   def amount_separated_by_rate(amount:, start_on:, end_on:)
-    # You can convert Integer/BigDecimal/Float/String/Rational classes to Rational,
-    # but the `amount` keyword argument does not accept BigDeciaml, Float and String in for the following reasons.
-    #   - Rational objects may be implicitly converted to BigDecimal or Float type 
-    #     when performing arithmetic operations using BigDecimal and Rational, or Float and Rational.
-    #   - String is not accepted because an exception is raised by data that cannot be converted, such as 1.1.1, for example.
-    raise ArgumentError.new('amount data-type must be Integer or Rational') unless amount.is_a?(Integer) || amount.is_a?(Rational)
-    raise ArgumentError.new('start_on data-type must be Date') unless start_on.is_a?(Date)
-    raise ArgumentError.new('end_on data-type must be Date') unless end_on.is_a?(Date)
+    amount = Amount.new(amount)
+    raise ArgumentError.new('amount must be greater than or equal to zero') if amount.value < 0
 
-    # By using the modified Julian date, we can handle all Date as Integer. This speeds up the process.
-    start_on_mjd = start_on.mjd
-    end_on_mjd = end_on.mjd
-
-    raise ArgumentError.new('start_on must not be after than end_on') if start_on_mjd > end_on_mjd
-    raise ArgumentError.new('start_on must bigger than 1873/1/1') if start_on_mjd < EXCISE_HASHES.first[:start_on].mjd
-    raise ArgumentError.new('amount must be greater than or equal to zero') if amount < 0
-
-    # Use the number of days until end_on_mjd.
-    daily_amount = Rational(amount, (start_on_mjd..end_on_mjd).count)
+    term = Term.new(start_on: start_on, end_on: end_on)
+    raise ArgumentError.new('start_on must bigger than 1873/1/1') if start_on < EXCISE_TAX_RATES.first.term.start_on
 
     {}.tap do |return_hash|
-      EXCISE_HASHES.inject(0) do |sum, hash|
-        # It determines whether there are overlapping periods by comparing the start and end dates of a certain consumption tax with 
-        # the start and end dates of the period for which the tax-inclusive price is to be calculated this time.
-        # If there is an overlap, the price for the subject period is calculated by multiplying the number of days of the overlapping period
-        # by the pro rata amount.
-        larger_start_on_mjd = [start_on_mjd, hash[:start_on].mjd].max
-        smaller_end_on_mjd = [end_on_mjd, hash[:end_on].mjd].min
+      EXCISE_TAX_RATES.each do |excise_tax_rate|
+        next unless excise_tax_rate.is_in_effect_for?(term)
 
-        # Check if there is an overlapping period
-        if larger_start_on_mjd <= smaller_end_on_mjd
-          # Number of days of overlapping period
-          number_of_days_in_this_excise_rate_term = (larger_start_on_mjd..smaller_end_on_mjd).count
-          return_hash[hash[:rate]] = (daily_amount * number_of_days_in_this_excise_rate_term).truncate
-        end
+        return_hash[excise_tax_rate.rate] = (
+          amount.per_day(term.number_of_days) *
+          term.number_of_days_that_overlap_with(excise_tax_rate.term)
+        ).truncate
       end
 
-      # If the divided amount is not divisible by the number of target tax rates, 
+      # If the divided amount is not divisible by the number of target tax rates,
       # the sum of the amount in the argument and the divided amount may be less than the actual value.
       # This is because the undivided value is truncated at the time of division.
       # e.g.
-      #    amount: 100000, start_on: 1997/3/31, end_on 2014/4/1の場合
+      #    amount: 100000, start_on: 1997/3/31, end_on 2014/4/1
       #    3%:16
       #    5%:99_967
       #    8%:16
       #    => 16+99967+16=99999
       # Add the amount that is out of alignment to the amount that belongs to the lowest sales tax amount
       # to equal the sum of the argument amount and the divided amount.
-      # The reason for adding the shortfall to the amount that belongs to the least amount of consumption tax 
+      # The reason for adding the shortfall to the amount that belongs to the least amount of consumption tax
       # is so that the user will have an advantage when the consumption tax is calculated based on this amount.
       # Example 1
-      #    amount: 100000, start_on: 1997/3/31, end_on 2014/4/1の場合
+      #    amount: 100000, start_on: 1997/3/31, end_on 2014/4/1
       #    3%:17 <- Actually 16, but add 1 yen.
       #    5%:99_967
       #    8%:16
       #    => 17+99967+16=100000
       #
       # Example 2:
-      #    amount: 100000, start_on: 2014/3/31, end_on 2019/10/1の場合
+      #    amount: 100000, start_on: 2014/3/31, end_on 2019/10/1
       #    5%:51 <- Actually 49, but add 2 yen.
       #    8%:99_900
       #    10%:49
@@ -140,24 +102,15 @@ module Jct
       #
       # FIXME: `Enumerable#sum` has been supported since ruby 2.4, but this gem uses `reduce` because it still needs to support ruby 2.3 series.
       summarize_separated_amount = return_hash.each_value.reduce(&:+)
-      if amount != summarize_separated_amount
-        return_hash[return_hash.each_key.min] += (amount - summarize_separated_amount)
+      if amount.value != summarize_separated_amount
+        return_hash[return_hash.each_key.min] += (amount.value - summarize_separated_amount)
       end
     end
   end
 
   def rate(date = Date.today)
-    case date
-    when Date.new(1989, 4, 1)..Date.new(1997, 3, 31)
-      RATE103
-    when Date.new(1997, 4, 1)..Date.new(2014, 3, 31)
-      RATE105
-    when Date.new(2014, 4, 1)..Date.new(2019, 9, 30)
-      RATE108
-    when Date.new(2019, 10, 1)..Date::Infinity.new
-      RATE110
-    else
-      RATE100
-    end
+    EXCISE_TAX_RATES.find {
+      |excise_tax_rate| excise_tax_rate.is_in_effect_on?(date)
+    }.rate || RATE100
   end
 end
